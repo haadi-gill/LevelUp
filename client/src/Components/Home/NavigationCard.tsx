e9cee834f951f0971b27a257c3770a1a44751458
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-04-21 19:56:10 -0400

    added more routing....

--- client/src/Components/Home/NavigationCard.tsx
+++ client/src/Components/Home/NavigationCard.tsx
@@ -8,12 +8,12 @@ export default function NavigationCard(){
                 <img src={icon} alt="icon" style={{ width: '40px', height: '40px' }}/>
                 <h2 className="text-3xl font-semibold mb-3">LevelUp</h2>
                 </div>
-                <a href="" className="flex gap-3 py-3 bg-indigo-600 text-white -mx-10 px-10 rounded-xl shadow-md shadow-gray-700 hover:scale-105 transition-all">
+                <a href="/HomePage" className="flex gap-3 py-3 bg-indigo-600 text-white -mx-10 px-10 rounded-xl shadow-md shadow-gray-700 hover:scale-105 transition-all">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
                 </svg>
                 Home</a>
-                <a href="" className="flex gap-3 py-3 hover:bg-indigo-300 -mx-10 px-10 rounded-xl transition-all hover:scale-105 hover:shadow-md shadow-gray-700">
+                <a href="/Dashboard" className="flex gap-3 py-3 hover:bg-indigo-300 -mx-10 px-10 rounded-xl transition-all hover:scale-105 hover:shadow-md shadow-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
                     </svg>
