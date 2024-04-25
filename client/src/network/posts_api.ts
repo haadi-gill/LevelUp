8f56809d027ae7eda393a11e1697d8df9cb2449b
Manas Adepu <112828012+manasadepu@users.noreply.github.com>
2024-04-24 22:36:06 -0400

    added like feature and logout feature

--- client/src/network/posts_api.ts
+++ client/src/network/posts_api.ts
@@ -1,5 +1,5 @@
 import { fetchData } from "./fetch";
-import { Post, getPosts } from "../models/posts";
+import { Post, getPostAfterUpdate, getPosts, updateLiked } from "../models/posts";
 
 
 export interface createPost {
@@ -7,6 +7,7 @@ export interface createPost {
     task: string;
     photos?: string;
     user_id?: string;
+    likes?: string[];
 }
 
 export const createPost = async (post: createPost) => {
@@ -24,6 +25,19 @@ export const createPost = async (post: createPost) => {
 export async function getAllPosts(): Promise<getPosts> {
     const response = await fetchData("http://localhost:5000/api/posts/allposts", {
         method: "GET"
+    });
+
+    return response.json();
+}
+
+export async function updateLikes(params: updateLiked): Promise<getPostAfterUpdate> {
+
+    const response = await fetchData("http://localhost:5000/api/posts/update/likes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ postID: params.postID, userID: params.userID })
     });
 
     return response.json();
